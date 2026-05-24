c6093a1a8a27e8e478b861b507c5a8734c49a325
Debra-7 <ndukudebrah07@gmail.com>
2026-05-25 01:44:56 +0300

    Completed Python Assignment

--- python_fundamentals/vars.py
+++ python_fundamentals/vars.py
@@ -51,6 +51,78 @@ my_name = getName("Marie")
 print(f"My name is {my_name}")
 getGrade(marks)
 
+getClasses(classes)
+
 # 1. Prints out my majors
+
+#This function prints out the majors I'm doing in one line using the join method.
+def print_majors(majors):
+    if len(majors) == 1:
+        print(f"I am doing {majors[0]} as a major.")
+    else:
+        all_but_last = ", ".join(majors[:-1])
+        print(f"I am doing {all_but_last} and {majors[-1]} as majors.")
+
 # 2. Prints out my Minors
+
+#This function uses a recursive approach to flatten out the nested mminors tuple.
+def flatten(items):
+    flattened = []
+    for item in items:
+        if isinstance(item, (list, tuple)):
+            flattened.extend(flatten(item))
+        else:
+            flattened.append(item)
+    return flattened
+
+#This function prints out the minors I'm doing in one line using the join method.
+def print_minors(minors):
+    flattened_minors = flatten(minors)
+    if len(flattened_minors) == 1:
+        print(f"I am doing {flattened_minors[0]} as a minor.")
+    else:
+        all_but_last = ", ".join(flattened_minors[:-1])
+        print(f"I am doing {all_but_last} and {flattened_minors[-1]} as minors.")
+
 # 3. Use a while loop to ask the user to enter the information they want and close when the user types close
+
+#While loop to ask the user to enter the information they want and close when the user types close
+def user_input():
+    print("\nType one of the following to get information:")
+    print(" 'name' - Get name")
+    print(" 'profile' - Get profile")
+    print(" 'classes' - Get classes")
+    print(" 'marks' - Get marks")
+    print(" 'grade' - Get grade")
+    print(" 'majors' - Get majors")
+    print(" 'minors' - Get minors") 
+    print(" 'close' - Exit")
+
+    while True:
+        user_choice = input("\nEnter what you want to see: ").lower()
+        
+        if user_choice == "close":
+            print("Exiting the program. Goodbye!")
+            break
+
+        elif user_choice == "name":
+            print(f"My name is {my_name}")
+        elif user_choice == "profile":
+            getProfile(my_name, age, location, country, date_of_birth)
+        elif user_choice == "classes":
+            getClasses(classes)
+        elif user_choice == "marks":
+            getMarks(marks)
+        elif user_choice == "grade":
+            getGrade(marks)
+        elif user_choice == "majors":
+            print_majors(majors)
+        elif user_choice == "minors":
+            print_minors(minors)
+        
+        else:
+            print("Invalid choice. Please try again.")
+
+print_majors(majors)
+print_minors(minors)
+user_input()
