db833467dfcacfc157cadb4028bc6ece4e6521f2
quiesscent <ephesianslewis@gmail.com>
2026-05-30 15:50:09 +0300

    initialized hpf_assignment

--- hpf_python_fundamentals/initial.py
+++ hpf_python_fundamentals/initial.py
@@ -105,16 +105,4 @@ while(run):
     #     print(likes_dislikes[val])
 
 
-"""
-Write a python using function, loops, operators and conditionals to get the numbers that are not divisible by 3, 7, 11 ranging from 1-150
-Do not print the numbers one by one instead, assign or add them to a list or a tuple and print the list or the tuple
 
-Bonus task:
-Assign the remaining numbers in their own list or tuple then create a dictionary with the below structure
-my_finding = {
-    "divisible_by_3": [...],
-    "divisible_by_7": [...],
-    "divisible_by_11": [...],
-    "not_divisible_by_3_7_11": [...]
-}
-"""
